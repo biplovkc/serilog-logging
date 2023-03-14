f2776cba28ce3338796aaffdf24d52e2c93f107e
Biplov <biplov.cybercop@gmail.com>
2023-03-14 12:38:17 +0100

    override updates

--- src/LoggingServiceCollectionExtension.cs
+++ src/LoggingServiceCollectionExtension.cs
@@ -2,7 +2,37 @@
 
 public static class LoggingServiceCollectionExtension
 {
-    public static IServiceCollection RegisterSerilogLogger(this IServiceCollection services, IConfiguration configuration, string version, Dictionary<string, LogEventLevel> overrides = null, params string[] pathsToExclude)
+    public static IServiceCollection RegisterSerilogLogger(this IServiceCollection services, IConfiguration configuration, string version, params string[] pathsToExclude)
+    {
+        var loggerConfig = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration);
+
+        var logger = loggerConfig
+            .Enrich.FromLogContext()
+            .Enrich.WithAssemblyName()
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithSpan();
+
+        if (!string.IsNullOrWhiteSpace(version))
+        {
+            logger.Enrich.WithProperty("Version", version);
+        }
+
+        if (pathsToExclude.Any())
+        {
+            foreach (var path in pathsToExclude)
+            {
+                logger.Filter.ByExcluding(path);
+            }
+        }
+
+        Log.Logger = logger.CreateLogger();
+        services.AddSingleton(Log.Logger);
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterSerilogLogger(this IServiceCollection services, IConfiguration configuration, string version, Dictionary<string, LogEventLevel> overrides, params string[] pathsToExclude)
     {
         var loggerConfig = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration);
